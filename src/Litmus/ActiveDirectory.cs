ade947a13b89749c6303e890e263d0b74efc4329
derekscode <derekscode@github.com>
2016-04-26 18:47:44 -0700

    initial commit

---
+++ src/Litmus/ActiveDirectory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+
+namespace Litmus
+{
+    public class ActiveDirectory
+    {
+        //var test = User.IsInRole("S-1-1-0");
+
+        public const string CORPecoQADBRead_G = "S-1-5-21-102932503-109117628-3773961456-41001";
+        public const string ecoATMLitmusUser = "";
+        public const string ecoATMLitmusAdmin = "";
+
+
+        // returns all AD groups with corresponding SIDs
+        public static Dictionary<string, string> GetGroups(HttpContext context)
+        {
+
+            var dictionary = new Dictionary<string, string>();
+            var groups = ((WindowsIdentity)context.User.Identity).Groups;
+
+            foreach (var group in groups)
+            {
+                dictionary.Add(group.Value, ConvertSidToGroup(group.Value));
+            }
+
+            return dictionary;
+        }
+
+
+        private static string ConvertSidToGroup(string sid)
+        {
+            string group = new SecurityIdentifier(sid).Translate(typeof(NTAccount)).ToString();
+            return group;
+        }
+    }
+}
--- src/Litmus/Controllers/HomeController.cs
+++ src/Litmus/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
 
 namespace Litmus.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize(Roles = ActiveDirectory.CORPecoQADBRead_G)]
         public IActionResult Index()
         {
             return View();
